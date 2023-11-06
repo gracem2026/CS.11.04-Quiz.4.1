77a3817ceb3334f2371bd685410a71c22c885a97
gracema <gracem2026@student.cis.edu.hk>
2023-11-06 12:57:18 +0800

    i failed

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,14 @@ public class Main {
 
     /** 1. Write a method called numberOfVowels that returns the number of vowels in a string.
      Consider a, e, i, o, u as vowels. **/
-    
+    public static int numberOfVowels(String str){
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            String temp = str.substring(i, i+1);
+            if(temp.equals("a") || temp.equals("e") || temp.equals("i") || temp.equals("o") || temp.equals("u")) count ++;
+        }
+        return count;
+    }
 
     /** 2. Write a method called notDivisibleBy235 that finds the number of natural numbers not exceeding N
      and not divisible by any of the numbers [2, 3, 5].
@@ -14,7 +21,13 @@ public class Main {
      4 - divides integer by 2
      5 - divides integer by 5
      Answer: 1 (only one number doesn't divide integer by any of 2, 3, 5) **/
-
+    public static int notDivisibleBy235(int n) {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (!(i % 2 == 0) && !(i % 3 == 0) && !(i % 5 == 0)) count ++;
+        }
+        return count;
+    }
 
     /** 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing.
      * The first word within the output should be capitalized only if the original word was capitalized
@@ -24,6 +37,29 @@ public class Main {
      "the-stealth-warrior" gets converted to "theStealthWarrior"
      "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
      "The_Stealth-Warrior" gets converted to "TheStealthWarrior" **/
+    public static String camelCaseMe(String str) {
+        String result = "";
+        int startIndex = 0;
+        int endIndex = 0;
 
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '-' || str.charAt(i) == '_' && (str.charAt(i) == 0)) {
+                String upperChar = str.substring(0, 1).toUpperCase();
+                int end = 0;
+                for (int h = 1; h < str.length(); h++) {
+                    if (str.charAt(h) == '-' || str.charAt(h) == '_') end = h;
+                }
+                String tempWord = str.substring(1, end);
+            }
+            else if ((str.charAt(i) != 0)) {
+                String upperChar = str.substring(startIndex, startIndex+1).toUpperCase();
+                String tempWord = str.substring(startIndex+1, i);
+                result = result + upperChar + tempWord;
+            }
+
+        }
+
+        return result;
+    }
 
 }
